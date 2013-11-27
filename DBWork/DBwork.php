c7671394b85564ce97038b0fd80c4260f41ca3d8
Ilan Frumer <ilanfrumer@gmail.com>
2013-11-27 21:05:40 +0200

    DBWork.php fixed psr2

--- DBWork/DBwork.php
+++ DBWork/DBwork.php
@@ -9,58 +9,54 @@ use PDOExeption;
 class DBWork
 {
 
-	private $dbh;
-	private $config;
-	private $valid;
+    private $dbh;
+    private $config;
+    private $valid;
 
-	public function __construct() {
+    public function __construct()
+    {
 
-		$this->config = new Config();
-		$this->valid = new CheckValid();
+        $this->config = new Config();
+        $this->valid = new CheckValid();
 
-		$dbc = 'mysql:host=' . $this->config->host . ';dbname=' . $this->config->dbName;
-		$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        $dbc = 'mysql:host=' . $this->config->host . ';dbname=' . $this->config->dbName;
+        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-		try {
-			
-			$this->dbh = new PDO($dbc.';charset=utf8',$this->config->userName,$this->config->password,$options);
+        try {
+            $this->dbh = new PDO($dbc.';charset=utf8', $this->config->userName, $this->config->password, $options);
 
-		} catch (PDOException $e) {
+        } catch (PDOException $e) {
 
-			echo $e->getMessage();
-		}
-	}
-
-
-
-	private function fieldClause($fields) 
-	{
-
-		// var_dump($alias);
-		// var_dump($fields);
-
-		// if(is_array($fields)) {
-		// 	$alias = $alias;
-		// 	$fields = array_map(function($field) use ($alias) {
-		// 		return" $alias.`$field`";
-		// 	},$fields);
-
-		// }
-
-		$fields = implode(",", $fields);	
-		return $fields;
-
-	}
-
-	public function select($table, $alias, $fields)
-	{
-		$fields = $this->fieldClause($fields, $alias);
-			
-		 return new SelectQuery($this->dbh, $table, $alias, $fields);
-		
-	}
+            echo $e->getMessage();
+        }
+    }
 
 
+
+    private function fieldClause($fields)
+    {
+
+        // var_dump($alias);
+        // var_dump($fields);
+
+        // if(is_array($fields)) {
+        //  $alias = $alias;
+        //  $fields = array_map(function($field) use ($alias) {
+        //      return" $alias.`$field`";
+        //  },$fields);
+
+        // }
+
+        $fields = implode(",", $fields);
+        return $fields;
+
+    }
+
+    public function select($table, $alias, $fields)
+    {
+        $fields = $this->fieldClause($fields, $alias);
+            
+         return new SelectQuery($this->dbh, $table, $alias, $fields);
+        
+    }
 }
-
-
